ef7a63aaa3fc07fc14becde2c7a185cc8efbf424
Alejandro Decaroli <alejandro.decaroli.97@gmail.com>
2023-04-27 17:28:30 -0300

    Actualizacion de graficos de seaborn

--- seaborn/sns.chart.py
+++ seaborn/sns.chart.py
@@ -1,6 +1,16 @@
 import seaborn as sns
 import matplotlib.pyplot as plt 
+'''
+Seaborn esta diseñado sobre matplotlib para trabajar de forma mas eficiente con dataframes de pandas
 
+Seaborn tiene 4 formas de hacer graficos cada uno con sus opciones segun el tipo de informacion a visualizar.
+Esta el joinplot() que combina varias clases o kind de graficos en uno solo para mostrar toda la informacion rapidamente (hace graficos chetos)
+Esta el replot() que es relationship esta el scatterplot y el lineplot
+Esta el displot de distribution con kdeplot, histplot, ecdfplot, rugplot.
+y el catplot de categorical con stripplot, swarmplot, boxplot, violinplot, pointplot y barplot
+
+para graficar eligo el tipo de grafico y luego la clase 
+'''
 print()
 print(sns.get_dataset_names()) # Muestra lista de nombre de los data sets de ejemplo
 print()
@@ -8,9 +18,17 @@ print()
 planets = sns.load_dataset('planets')
 print(planets)
 
-sns.displot(data=planets, x='mass', y='orbital_period', hue='method', legend=True, kind='kde')
-# displot elige el tipo de grafico mas eficiente segun seaborn
-# Hue relaciona las dos variables con una tercera asignandole un color y la leyenda
-# kind elige el tipo de grafico
-# Inicialmente es un grafico de distribucion de una variable, pero al poner 2 automaticamente seaborn elige otra mejor representacion
+# Para graficos de una variable
+sns.displot(data=planets, x='mass', legend=False, cumulative=False, kind='hist', stat='count', hue='method',multiple='dodge')
+sns.displot(data=planets, x='mass', kind='kde', hue='method')
 plt.show()
+
+'''
+# El cumulative acumula los valores para generar los siguientes
+# El stat elige como es la escala del eje y, puede ser 'count', 'percent', 'frequency' o 'probability'
+# multiple es la forma en la que se comparan los datos en el eje x, dodge: se pone una columna al lado de la otra, en stack se apilan 
+  y en layer se acumulan pero se diferencian
+# Hue relaciona las variables con una tercera asignandole un color y la leyenda
+# kind elige el tipo de grafico
+# Shade = True sirve para graficar el area debajo de la curva en el grafico kde, solo funciona si uso kdeplot especificamente
+'''
